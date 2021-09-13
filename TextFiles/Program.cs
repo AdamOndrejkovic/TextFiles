fa44a28e54ae7ee70a00ab7f69dc1a05619d4a25
Adam Ondrejkovic <adam.ondrejkovic@gmail.com>
2021-09-13 18:53:35 +0200

    Initial commit

---
+++ TextFiles/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace TextFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Users\adamo\RiderProjects\TextFiles\TextFiles\Text.txt");
+
+            Console.WriteLine($"Text file contains following text: {text}");
+
+            string[] linesToAdd = { "Some random", "text that", "will be add", "to text file" };
+            
+            File.WriteAllLines(@"C:\Users\adamo\RiderProjects\TextFiles\TextFiles\Text.txt",linesToAdd);
+
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\adamo\RiderProjects\TextFiles\TextFiles\Text.txt");
+
+            foreach (var line in lines)
+            {
+                Console.WriteLine($"{line}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
